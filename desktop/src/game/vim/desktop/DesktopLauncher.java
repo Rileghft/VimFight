e4ae97e0c4354eae2f3e99d508e3c1bbf9fc65dc
YANG, SHUN-YU <mywonderemailyoung@gmail.com>
2016-05-21 19:47:50 +0800

    initial version

---
+++ desktop/src/game/vim/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.vim.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.vim.VimFight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VimFight(), config);
+	}
+}
