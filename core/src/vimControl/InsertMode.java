fb6b14d53bf0e30efc42dc631442211c7a9c81b0
YANG, SHUN-YU <mywonderemailyoung@gmail.com>
2016-06-08 22:27:39 +0800

    insertMode

---
+++ core/src/vimControl/InsertMode.java
@@ -0,0 +1,43 @@
+/**
+ *
+ */
+package vimControl;
+
+import com.badlogic.gdx.ai.fsm.DefaultStateMachine;
+import com.badlogic.gdx.ai.fsm.StateMachine;
+
+/**
+ * @author 楊舜宇
+ * @since 2016/5/29
+ *
+ */
+public class InsertMode implements Mode{
+	public StateMachine<InsertMode, InsertState> insert;
+	public int inputKey;
+	private VimControl modeControl;
+
+	public InsertMode(VimControl mode) {
+		insert = new DefaultStateMachine<InsertMode, InsertState>(this, InsertState.TYPING);
+		modeControl = mode;
+	}
+
+	public void input(int keycode) {
+		inputKey = keycode;
+		insert.update();
+	}
+
+	public int getKey() {
+		return inputKey;
+	}
+
+	public void type(String inputChar) {
+		System.out.println(String.format("輸入字元： %s",  inputChar));
+	}
+
+	public void exit(int endKey) {
+		System.out.println("離開Insert Mode，回到Normal Mode");
+		modeControl.inputKey(inputKey);
+		inputKey = -1;
+	}
+
+}
