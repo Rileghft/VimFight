aa5c2aa51faf02160fe773fdb36f8292ef51d9aa
fnsne <watashiwafnsne@gmail.com>
2016-06-08 22:45:23 +0800

    some window setting of desktop

--- desktop/src/game/vim/desktop/DesktopLauncher.java
+++ desktop/src/game/vim/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import game.vim.VimFight;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "vim fighter";
+		config.width = 700;
+		config.height = 700;
+		config.useGL30 = true;
+		config.resizable = false;
 		new LwjglApplication(new VimFight(), config);
 	}
 }
