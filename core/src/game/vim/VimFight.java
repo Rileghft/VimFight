7f06293aebaeca681ec6248867c4d6c2a8cbd452
YANG, SHUN-YU <mywonderemailyoung@gmail.com>
2016-06-09 11:37:23 +0800

    加入stage

--- core/src/game/vim/VimFight.java
+++ core/src/game/vim/VimFight.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
+import game.Object.Player;
 import game.managers.GameInputProcessor;
 import game.managers.GameStateManager;
 
@@ -15,31 +18,39 @@ public class VimFight extends ApplicationAdapter {
 	public static int WIDTH;
 	public static int HEIGHT;
 	private GameStateManager gsm;
-	
+	private Stage stage;
+
 	@Override
 	public void create () {
 		WIDTH = Gdx.graphics.getWidth();
 		HEIGHT = Gdx.graphics.getHeight();
-		
+
 		cam = new OrthographicCamera(WIDTH,HEIGHT);
 		cam.translate(WIDTH/2, HEIGHT/2);
 		cam.update();
+		ScreenViewport viewport = new ScreenViewport();
+		stage = new Stage(viewport);
 		//here to set actor for input Proecessor
-		GameInputProcessor gameInputProcessor = new GameInputProcessor();
-		Gdx.input.setInputProcessor(gameInputProcessor);
-		
+		Gdx.input.setInputProcessor(stage);
+		Player player = new Player();
+		stage.addActor(player);
+		stage.setKeyboardFocus(player);
+
 		gsm = new GameStateManager();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(0.95f, 0.95f, 0.95f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.draw();
 	}
-	
+
 	public void resize( int width, int height ) {}
 	public void pause() {}
 	public void resume() {}
