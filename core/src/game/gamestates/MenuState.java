e50fa53a3d10749138abf1d668b444972ad1ee21
fnsne <watashiwafnsne@gmail.com>
2016-06-23 00:46:34 +0800

    make some animation test in menuState

--- core/src/game/gamestates/MenuState.java
+++ core/src/game/gamestates/MenuState.java
@@ -3,10 +3,17 @@ package game.gamestates;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.utils.Array;
 
 import game.managers.GameStateManager;
 import game.vim.VimFight;
@@ -19,10 +26,42 @@ public class MenuState extends GameState {
 	private SpriteBatch sb;
 	private BitmapFont titleFont;
 	private BitmapFont font;
+	//for test animation
+	private TextureAtlas charset;
+	private static float FRAME_DURATION = .05f;
+	private Animation runningAnimation;
+	private TextureRegion currentFrame;
+	private float elapsed_time = 0f;
+	private float origin_x, origin_y;
 	
 	public MenuState(GameStateManager gsm) {
 		super(gsm);
 		// TODO Auto-generated constructor stub
+		animationInit();
+	}
+
+//for test animation
+	private void animationInit(){
+		charset = new TextureAtlas(Gdx.files.internal("images/charset.atlas"));
+		
+		Array<AtlasRegion> runningFrames = charset.findRegions("running");
+		
+		runningAnimation = new Animation(FRAME_DURATION, runningFrames, PlayMode.LOOP);
+	
+		//calculate the x and y position to center the image	
+		origin_x = (Gdx.graphics.getWidth()/4*3 );
+		origin_y = (Gdx.graphics.getHeight()/4*3);
+	}
+	private void animationRender() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		elapsed_time += Gdx.graphics.getDeltaTime();
+		
+		currentFrame = runningAnimation.getKeyFrame(elapsed_time);
+		
+		sb.begin();
+		sb.draw(currentFrame, origin_x, origin_y);
+		sb.end();
 	}
 
 	@Override
@@ -61,7 +100,7 @@ public class MenuState extends GameState {
 	public void draw() {
 		// TODO Auto-generated method stub
 		sb.setProjectionMatrix(VimFight.cam.combined);
-		
+		animationRender();
 		sb.begin();
 		//draw title
 		titleFont.draw(sb, title, (VimFight.WIDTH - 50*title.length())/2, 600);
