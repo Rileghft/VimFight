1377497d4c820e834e7a891b9e5556f69a8a07b5
YANG, SHUN-YU <mywonderemailyoung@gmail.com>
2016-06-28 23:14:15 +0800

    新增移動 w, b, 0, $

--- core/src/game/Map/GameMap.java
+++ core/src/game/Map/GameMap.java
@@ -73,72 +73,194 @@ public class GameMap {
 		return screenRows;
 	}
 
-	public boolean moveRight(Position pos) {
+	public void updateScreenMap(Position pos) {
+  int nextScreenCol = screenStartCol;
+  int nextScreenRow = screenStartRow;
+  if(pos.x > screenStartCol + 19) {
+     nextScreenCol = pos.x - 1;
+  		}
+   else if(pos.x < screenStartCol) {
+       nextScreenCol = pos.x - 19;
+        }
+   if(pos.y > screenStartRow + 19) {
+       nextScreenRow = pos.y - 1;
+        }
+   else if(pos.y < screenStartRow) {
+       nextScreenRow = pos.y - 19;
+        }
+   if(nextScreenCol < 0) nextScreenCol = 0;
+   if(nextScreenRow < 0) nextScreenRow = 0;
+   screenStartCol = nextScreenCol;
+   screenStartRow = nextScreenRow;
+    }
+
+	public void moveRight(Position pos) {
 		int x = pos.x;
 		int y = pos.y;
-		boolean isUpdateEdge = false;
 		MapRow row = rows.get(y);
 		String line = row.getLineString();
 		if(x + 1 < line.length()) {
 			if(x + 1 >= screenStartCol + 20) {
 				screenStartCol++;
-				isUpdateEdge = true;
 			}
 			pos.x++;
 		}
-		return isUpdateEdge;
 	}
 
-	public boolean moveLeft(Position pos) {
+	public void moveLeft(Position pos) {
 		int x = pos.x;
-		boolean isUpdateEdge = false;
 		if(x - 1 >= 0) {
 			if(x - 1 < screenStartCol) {
 				screenStartCol--;
-				isUpdateEdge = true;
 			}
 			pos.x--;
 		}
-		return isUpdateEdge;
 	}
 
-	public boolean moveUp(Position pos) {
+	public void moveUp(Position pos) {
 		int y = pos.y;
-		boolean isUpdateEdge = false;
 		if(y - 1 >= 0) {
 			if(y - 1 < screenStartRow) {
 				screenStartRow--;
-				isUpdateEdge = true;
 			}
 			pos.y--;
 			MapRow row = rows.get(y - 1);
 			if(pos.x >= row.getLineString().length()) {
 				pos.x = row.getLineString().length() - 1;
+				if(pos.x < 0) pos.x = 0;
 			}
 		}
-		return isUpdateEdge;
 	}
 
-	public boolean moveDown(Position pos) {
+	public void moveDown(Position pos) {
 		int x = pos.x;
 		int y = pos.y;
-		boolean isUpdateEdge = false;
 		if(y + 1 < rows.size()) {
 			if(y + 1 >= screenStartRow + 20) {
 				screenStartRow++;
-				isUpdateEdge = true;
 			}
 			pos.y++;
 			MapRow row = rows.get(y + 1);
 			if(pos.x >= row.getLineString().length()) {
 				pos.x = row.getLineString().length() - 1;
+				if(pos.x < 0) pos.x = 0;
 			}
 		}
-		return isUpdateEdge;
 	}
 
 	public void moveNextWord(Position pos) {
 		int x = pos.x;
 		int y = pos.y;
+   int startLineCol = x;
+   int nextPosX = x;
+   int nextPosY = y;
+   Character curChar = rows.get(y).getLineString().charAt(x);
+   boolean isBreakPoint = false;
+
+   int curType;
+   if(curChar.toString().matches("\\s")) curType = 0;
+   else if(curChar.toString().matches("\\w")) curType = 1;
+   else curType = 2;
+
+   for(int rowNum = y; rowNum < rows.size(); ++rowNum) {
+       MapRow row = rows.get(rowNum);
+       String line = row.getLineString();
+       boolean isFound = false;
+       for (int i = startLineCol; i < line.length(); ++i) {
+           Character c = line.charAt(i);
+           if (c.toString().matches("\\s")) {
+               isBreakPoint = true;
+               continue;
+           } else if (c.toString().matches("\\w") && c != '_' && curType != 1 || isBreakPoint) {
+               isFound = true;
+               nextPosX = i;
+               nextPosY = rowNum;
+               break;
+           } else if (c.toString().matches("\\p{Punct}") && curChar != c && curType != 2 || isBreakPoint) {
+               isFound = true;
+               nextPosX = i;
+               nextPosY = rowNum;
+               break;
+                			}
+            	}
+      if(isFound) {
+          break;
+            	}
+      startLineCol = 0;
+        }
+   pos.x = nextPosX;
+   pos.y = nextPosY;
 	}
+
+	public void movePreWord(Position pos) {
+		int x = pos.x;
+		int y = pos.y;
+   int startLineCol = x;
+   int nextPosX = x;
+   int nextPosY = y;
+   Character curChar = rows.get(y).getLineString().charAt(x);
+   boolean isBreakPoint = false;
+
+   int curType;
+   if(curChar.toString().matches("\\s")) curType = 0;
+   else if(curChar.toString().matches("\\w") && curChar != '_') curType = 1;
+   else curType = 2;
+
+   for(int rowNum = y; rowNum >= 0; --rowNum) {
+       MapRow row = rows.get(rowNum);
+       String line = row.getLineString();
+       boolean isFound = false;
+       for (int i = startLineCol; i >= 0; --i) {
+           Character c = line.charAt(i);
+           if (c.toString().matches("\\s")) {
+               isBreakPoint = true;
+               continue;
+           } else if (c.toString().matches("\\w") && c != '_' && (curType != 1 || isBreakPoint)) {
+               isFound = true;
+               nextPosX = i;
+               nextPosY = rowNum;
+               curType = 1;
+               break;
+           } else if (c.toString().matches("\\p{Punct}") && (curChar != c || curType != 2 || isBreakPoint)) {
+               isFound = true;
+               nextPosX = i;
+               nextPosY = rowNum;
+               curType = 2;
+               break;
+                			}
+            		}
+       if(isFound) {
+           int startWordIndex = nextPosX;
+           for(int i = nextPosX; i >= 0; --i) {
+               Character c = line.charAt(i);
+               if (c.toString().matches("\\s")) {
+                   startWordIndex = i + 1;
+                   break;
+               } else if (c.toString().matches("\\w") && c != '_' && curType != 1) {
+                   startWordIndex = i + 1;
+                   break;
+               } else if (c.toString().matches("\\p{Punct}")  && curType != 2) {
+                   startWordIndex = i + 1;
+                   break;
+                    					}
+               if(i == 0) nextPosX = 0;
+                			}
+           nextPosX = startWordIndex;
+           break;
+            	}
+      startLineCol = (rowNum != 0)? rows.get(rowNum - 1).getLineString().length() - 1: rows.get(0).getLineString().length();
+        }
+   pos.x = nextPosX;
+   pos.y = nextPosY;
+    }
+
+	public void moveLineBegin(Position pos) {
+		pos.x = 0;
+	}
+
+	public void moveLineEnd(Position pos) {
+		String line = rows.get(pos.y).getLineString();
+		pos.x = (line.length() > 0)? line.length() - 1: 0;
+	}
+
 }
