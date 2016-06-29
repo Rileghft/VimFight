eaf729aa79c015b50a882bb3ea36a1553092001e
YANG, SHUN-YU <mywonderemailyoung@gmail.com>
2016-06-29 23:39:29 +0800

    修正人物移動問題

--- core/src/game/Object/CharacterAnimation.java
+++ core/src/game/Object/CharacterAnimation.java
@@ -12,7 +12,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 
 public class CharacterAnimation {
-	
+
 	private TextureAtlas charset;
 	private static float FRAME_DURATION = .1f;
 	private Animation downingAnimation;
@@ -27,69 +27,73 @@ public class CharacterAnimation {
 	private State currentState;
 	private State previousState;
 	private float dstX, dstY, vx, vy;
-	
+
 	public CharacterAnimation(String atlas){
 		charset = new TextureAtlas(Gdx.files.internal(atlas));
-		
+
 		Array<AtlasRegion> downFrames = charset.findRegions("downing");
 		Array<AtlasRegion> upFrames = charset.findRegions("uping");
 		Array<AtlasRegion> rightFrames = charset.findRegions("righting");
 		Array<AtlasRegion> leftFrames = charset.findRegions("lefting");
-		
-		
-		
+
+
+
 		downingAnimation = new Animation(FRAME_DURATION, downFrames, PlayMode.NORMAL);
 		upingAnimation = new Animation(FRAME_DURATION, upFrames, PlayMode.NORMAL);
 		rightingAnimation = new Animation(FRAME_DURATION, rightFrames, PlayMode.NORMAL);
 		leftingAnimation = new Animation(FRAME_DURATION, leftFrames, PlayMode.NORMAL);
-	
+
 		this.currentState = State.STANDING;
 		this.previousState = State.STANDING;
-		//calculate the x and y position to center the image	
-		origin_x = (Gdx.graphics.getWidth()/4*3 );
-		origin_y = (Gdx.graphics.getHeight()/4*3);
-		dstX = origin_x;
-		dstY = origin_y;
+		//calculate the x and y position to center the image
 		vx = 0;
 		vy = 0;
 	}
-	
+
 	public void setSpriteBatch( SpriteBatch sb ){
 		this.sb = sb;
 	}
-	
+
 	public void draw(){
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		float dt = Gdx.graphics.getDeltaTime();
 		elapsed_time += dt;
 		currentFrame = getFrame(elapsed_time);
-		if(vx > 0){
-			origin_x = (origin_x + dt*vx)> dstX ? dstX : origin_x+dt*vx;
-		}else{
-			origin_x = (origin_x + dt*vx)< dstX ? dstX : origin_x+dt*vx;
+		if(currentState == State.STANDING) {
+			origin_x = dstX;
+			origin_y = dstY;
 		}
-		if(vy > 0){
-			origin_y = (origin_y + dt*vy)> dstY ? dstY : origin_y+dt*vy;
-		}else{
-			origin_y = (origin_y + dt*vy)< dstY ? dstY : origin_y+dt*vy;
+		else {
+			if(vx > 0){
+				origin_x = (origin_x + dt*vx)> dstX ? dstX : origin_x+dt*vx;
+			}else{
+				origin_x = (origin_x + dt*vx)< dstX ? dstX : origin_x+dt*vx;
+			}
+			if(vy > 0){
+				origin_y = (origin_y + dt*vy)> dstY ? dstY : origin_y+dt*vy;
+			}else{
+				origin_y = (origin_y + dt*vy)< dstY ? dstY : origin_y+dt*vy;
+			}
 		}
 		sb.begin();
 		sb.draw(currentFrame, origin_x, origin_y);
 		sb.end();
 	}
-	
+
 	public void setOrgPos(float x, float y){
 		origin_x = x;
 		origin_y = y;
+		dstX = origin_x;
+		dstY = origin_y;
 	}
-	
+
 	public void setDstPos(float x, float y){
 		dstX = x;
 		dstY = y;
 		vx = (dstX-origin_x)/(float)0.3;
 		vy = (dstY-origin_y)/(float)0.3;
 	}
-	
+
 	private TextureRegion getFrame(float dt){
 		TextureRegion region;
 		switch (currentState) {
@@ -116,21 +120,21 @@ public class CharacterAnimation {
 			this.currentState = State.STANDING;
 		return region;
 	}
-	
+
 	//可以拿來在外部看到底走到位置了沒    因此可以用來讓人物移動不會走斜的
 	public boolean motionComplete(){
 		return (this.origin_x == this.dstX)&&(this.origin_y == this.dstY);
 	}
-	
+
 	public void startDown(){
 		elapsed_time = 0;
 		this.currentState = State.DOWNING;
 	}
-	
+
 	public void startUp(){
 		elapsed_time = 0;
 		this.currentState = State.UPING;
-		
+
 	}
 
 	public void startRight(){
@@ -142,7 +146,7 @@ public class CharacterAnimation {
 		elapsed_time = 0;
 		this.currentState = State.LEFTING;
 	}
-	
+
 	public void startJump(){
 		this.currentState = State.STANDING;
 		origin_x = dstX;
