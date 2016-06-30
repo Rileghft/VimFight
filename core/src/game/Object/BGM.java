1f729dd5be1f88c2ebae2a3792910e9e257a9771
fnsne <watashiwafnsne@gmail.com>
2016-06-30 13:14:37 +0800

    BGM change use Music

--- core/src/game/Object/BGM.java
+++ core/src/game/Object/BGM.java
@@ -1,22 +1,26 @@
 package game.Object;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
 public class BGM {
 	
-	private Sound BGM;
+	private Music BGM;
 	private boolean soundStoped;
 	
 	public BGM(String source){
-		BGM = Gdx.audio.newSound(Gdx.files.internal(source));
+		BGM = Gdx.audio.newMusic(Gdx.files.internal(source));
+		BGM.setLooping(true);
+		
 		soundStoped = true;
 	}
 	
 	public void startBGM() {
 		// TODO Auto-generated method stub
 		if(soundStoped){
-			BGM.loop();
+			BGM.play();
+			
 			soundStoped = true;
 		}
 	}
