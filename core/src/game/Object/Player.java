8bb592f631a1403fbacdb7288abf33772264c990
YANG, SHUN-YU <mywonderemailyoung@gmail.com>
2016-06-30 13:41:06 +0800

    整合f, F 尋找字元移動功能

--- core/src/game/Object/Player.java
+++ core/src/game/Object/Player.java
@@ -3,20 +3,16 @@
  */
 package game.Object;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
-import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
 
 import game.Map.GameMap;
 import game.Map.Position;
+import vimControl.GameKeys;
 
 /**
  * @author 楊舜宇
@@ -24,11 +20,12 @@ import game.Map.Position;
  *
  */
 public class Player extends Actor {
-	//Sprite sprite = new Sprite(new Texture(Gdx.files.internal("actor.png")));
 	private static float SQUARE_LENGTH = 30;
 	private Position pos;
 	private GameMap map;
 	private CharacterAnimation animation;
+	private boolean isFindCharState = false;
+	private int findCharDirection = 1;
 
 	public Player() {
 		//setBounds(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
@@ -41,81 +38,75 @@ public class Player extends Actor {
 
 		addListener(new InputListener(){
 			@Override
-			public boolean keyDown(InputEvent event, int keycode) {
-				RunnableAction ra = new RunnableAction();
-				switch (keycode) {
-				case Keys.J:
+			public boolean keyTyped(InputEvent event, char keyChar) {
+				if((int)keyChar == 0) return false;
+				if(isFindCharState) {
+					if(findCharDirection == 1)
+						map.moveFindChar(pos, keyChar);
+					else map.moveFindPreChar(pos, keyChar);
+					updateMap();
+					isFindCharState = false;
+					return true;
+				}
+				switch (keyChar) {
+				case GameKeys.j:
 					map.moveDown(pos);
 					animation.startDown();
 					break;
-				case Keys.K:
+				case GameKeys.k:
 					map.moveUp(pos);
 					animation.startUp();
 					break;
-				case Keys.H:
+				case GameKeys.h:
 					map.moveLeft(pos);
 					animation.startLeft();
 					break;
-				case Keys.L:
+				case GameKeys.l:
 					map.moveRight(pos);
 					animation.startRight();
 					break;
-				case Keys.W:
+				case GameKeys.w:
 					map.moveNextWord(pos);
 					animation.startJump();
 					break;
-				case Keys.B:
+				case GameKeys.b:
 					map.movePreWord(pos);
 					animation.startJump();
 					break;
-				case Keys.NUM_0:
+				case GameKeys.NUM_0:
 					map.moveLineBegin(pos);
 					animation.startJump();
 					break;
-				case Keys.NUM_4:
+				case GameKeys.DOLLAR:
 					map.moveLineEnd(pos);
 					animation.startJump();
+					break;
+				case GameKeys.f:
+					isFindCharState = true;
+					findCharDirection = 1;
+					break;
+				case GameKeys.F:
+					isFindCharState = true;
+					findCharDirection = 0;
 					break;
 				default:
 					break;
 				}
-				map.updateScreenMap(pos);
-				/*ra.setRunnable(new Runnable() {
-					@Override
-					public void run() {
-						movePlayer();
-					}
-				});
-				Player.this.addAction(ra);*/
-				int screenStartRow = map.screenStartRow;
-				int screenStartCol = map.screenStartCol;
-				int row = pos.y - screenStartRow;
-				int col = pos.x - screenStartCol;
-				animation.setDstPos(55f + col * SQUARE_LENGTH, 630f - row * SQUARE_LENGTH);
+				updateMap();
 
 				return true;
-			}
-
-			@Override
-			public boolean keyTyped(InputEvent event, char character) {
-
-				return super.keyTyped(event, character);
 			}
 		});
 
 	}
 
-	private void movePlayer() {
-		int screenStartRow = map.screenStartRow;
-		int screenStartCol = map.screenStartCol;
-		int row = pos.y - screenStartRow;
-		int col = pos.x - screenStartCol;
-		System.out.println(String.format("row=%d, col=%d", row, col));
-		setPosition(55f + col * SQUARE_LENGTH, 630f - row * SQUARE_LENGTH);
-	}
-
-	private boolean isEdge(float xPos, float yPos) {
-		return (xPos > 55f && xPos < 655f) || (yPos < 630f && yPos > 30f);
+	private void updateMap() {
+			map.updateScreenMap(pos);
+			int screenStartRow = map.screenStartRow;
+			int screenStartCol = map.screenStartCol;
+			int row = pos.y - screenStartRow;
+			int col = pos.x - screenStartCol;
+			animation.setDstPos(55f + col * SQUARE_LENGTH, 630f - row * SQUARE_LENGTH);
 	}
 
 	public void setMap(GameMap map) {
