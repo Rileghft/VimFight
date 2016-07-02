97dc3ee4c74c233676e4f7dafec3c30a80445d5a
fnsne <watashiwafnsne@gmail.com>
2016-07-03 00:26:53 +0800

    add how to play Graph

--- core/src/game/gamestates/HelpState.java
+++ core/src/game/gamestates/HelpState.java
@@ -3,6 +3,7 @@ package game.gamestates;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
@@ -17,6 +18,11 @@ public class HelpState extends GameState {
 	private SpriteBatch sb;
 	private final String hint = "Press ESCAPE to Go Back to Game";
 	private BitmapFont font;
+	private Texture hint1, hint2, left, right;
+	private float hintBegLeftX = 105;
+	private float hintBegUpY = 170;
+	private Texture currentHint;
+	private int selected ;
 	
 	protected HelpState(GameStateManager gsm, PlayState playState) {
 		super(gsm);
@@ -37,6 +43,13 @@ public class HelpState extends GameState {
 		itemParameter.size = 30;
 		font = gen.generateFont(itemParameter);
 		font.setColor(Color.YELLOW);
+		
+		hint1 = new Texture(Gdx.files.internal("images/HowToPlayP1.png"));
+		hint2 = new Texture(Gdx.files.internal("images/HowToPlayP2.png"));
+		left = new Texture(Gdx.files.internal("images/left.png"));
+		right = new Texture(Gdx.files.internal("images/right.png"));
+		currentHint = hint1;
+		selected = 0;
 	}
 
 	@Override
@@ -50,8 +63,12 @@ public class HelpState extends GameState {
 		// TODO Auto-generated method stub
 		sb.setProjectionMatrix(VimFight.cam.combined);
 		sb.begin();
-		font.draw(sb, hint, (VimFight.WIDTH - 15*hint.length())/2, 100);
-		System.out.println("hint.length() = "+hint.length());
+		sb.draw(currentHint, hintBegLeftX, hintBegUpY);
+		if(selected == 0)
+			sb.draw(right, hintBegLeftX+500, hintBegUpY+100);
+		if(selected == 1)
+			sb.draw(left, hintBegLeftX, hintBegUpY+100);
+		font.draw(sb, hint, (VimFight.WIDTH - 15*hint.length())/2-50, 200);
 		sb.end();
 	}
 
@@ -63,8 +80,34 @@ public class HelpState extends GameState {
 			gsm.setNeedClean(true);
 			gsm.pop();
 		}
+		if(Gdx.input.isKeyJustPressed(Keys.H)){
+			if( selected > 0 ){
+				selected--;
+				selectHint();
+			}
+		}
+		if(Gdx.input.isKeyJustPressed(Keys.L)){
+			if( selected < 1 ){
+				selected++;
+				selectHint();
+			}
+		}
+		
 	}
 
+	private void selectHint(){
+		switch (selected) {
+		case 0:
+			currentHint = hint1;
+			break;
+		case 1:
+			currentHint = hint2;
+			break;
+		default:
+			break;
+		}
+	}
+	
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
